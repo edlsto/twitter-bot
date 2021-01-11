99ca830745c10bd14df0b04d906c8faab781d272
Edward Stoner <edwardlstoner@gmail.com>
2021-01-11 15:35:35 -0700

    Add postgres

--- bot.py
+++ bot.py
@@ -1,30 +1,24 @@
 import tweepy
-import time
-import re
 import urllib.request
-import random
-import ast
 import os
-import sqlite3
+import psycopg2
 
 from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 from db_utils import get_random_photo
-conn = sqlite3.connect('photos.db')
+con = psycopg2.connect(database="postgres", user="postgres", password="", host="127.0.0.1", port="5432")
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-while True:
-    result = get_random_photo(conn)[0]
-    photo_id = result["id"]
-    urllib.request.urlretrieve(result['imageUrl'], f"images/{photo_id}.jpg")
-    twitter_API = tweepy.API(auth)
-    media = twitter_API.media_upload(f"images/{photo_id}.jpg")
-    summary = result["summary"][:200]
-    date = result["date"].split(" ")[0]
-    tweet = summary + " (" + date + ") " + result['pageUrl']
-    print(tweet)
-    twitter_API.update_status(status=tweet, media_ids=[media.media_id])
-    time.sleep(600)
+result = get_random_photo(con)
+photo_id = result["id"]
+urllib.request.urlretrieve(result['imageurl'], f"images/{photo_id}.jpg")
+twitter_API = tweepy.API(auth)
+media = twitter_API.media_upload(f"images/{photo_id}.jpg")
+summary = result["summary"][:200]
+date = result["date"].split(" ")[0]
+tweet = summary + " (" + date + ") " + result['pageurl']
+twitter_API.update_status(status=tweet, media_ids=[media.media_id])
+os.remove(f"images/{photo_id}.jpg")
 
 
--- db_utils.py
+++ db_utils.py
@@ -1,26 +1,43 @@
+import psycopg2.extras
+
 def create_photo(conn, image):
     sql = """
-        INSERT INTO photos(id, imageUrl, pageUrl, summary, creator, date) VALUES(?, ?, ?, ?, ?, ?)"""
+        INSERT INTO photos (id,imageUrl,pageUrl,summary,creator,date) VALUES (%s, %s, %s, %s, %s, %s)
+    """
     cur = conn.cursor()
-    cur.execute(sql, (image["id"], image["imageUrl"], image["pageUrl"], image["summary"], image["creator"], image["date"]))
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
+    data = (image["id"], image["imageUrl"], image["pageUrl"], image["summary"], image["creator"], image["date"])
+    print(type(data))
+    cur.execute(sql, data)
+    print("Table created successfully")
+    conn.commit()
 
 def get_random_photo(conn):
-    sql = """
-        SELECT * FROM photos
-        WHERE _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM photos))
-        LIMIT 1
-        """
-    conn.row_factory = dict_factory
+    sql = """ 
+        SELECT COUNT(*) FROM photos
+    """
     cur = conn.cursor()
     cur.execute(sql)
-    return cur.fetchall()
+    result = str(cur.fetchone()[0])
+    sql = """
+        SELECT * FROM photos OFFSET floor(random()*%s) LIMIT 1;
+    """
+    dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    dict_cur.execute(sql, (result,))
+    return dict_cur.fetchone()
 
-
-
+def get_all_photos(conn):
+    sql = """
+        SELECT * FROM photos
+    """
+    dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    dict_cur.execute(sql)
+    return dict_cur.fetchall()
+    
+def delete_photo(conn, _id):
+    sql = """
+            DELETE FROM photos WHERE id = %s;
+        """
+    cur = conn.cursor()
+    cur.execute(sql, (_id,))
+    conn.commit()
+    print('deleted')
