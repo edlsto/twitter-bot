969b67f12df1ac3d4f94ba00daa118e1e77fddb9
Edward Stoner <edwardlstoner@gmail.com>
2023-06-17 15:16:59 -0600

    v2 to tweet

--- bot.py
+++ bot.py
@@ -36,47 +36,48 @@ def extract_date(string):
     return f"({result})"
 
 con = psycopg2.connect(DATABASE_URL, sslmode='require')
-# con = psycopg2.connect(database="postgres", user="postgres", password="", host="127.0.0.1", port="5432")
 
-#Authenticate to the Twitter API
+# con = psycopg2.connect(database="historic_photos", user="edwardstoner", password="", host="127.0.0.1", port="5432")
+
+#v1 auth
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 twitter_API = tweepy.API(auth)
 
-# Get the user timeline
-tweets = []
-for status in tweepy.Cursor(twitter_API.user_timeline, screen_name='@colohistory', tweet_mode="extended").items():
-    tweets.append(status)
-ids = []
-for tweet in tweets:
-    ids.append(tweet.entities['urls'][0]['expanded_url'].split("/")[-1])
+#v2 auth
+client = tweepy.Client(
+    consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_KEY, access_token_secret=ACCESS_SECRET
+)
 
 # Get the current time. Tweet if it's an even hour
 now = datetime.datetime.now()
-if now.hour % 2 == 0:
 
-    # Get a random photo
-    result = get_random_photo(con, ids)
-    photo_id = result["id"]
+# Get a random photo
+result = get_random_photo(con)
+photo_id = result["id"]
 
-    # Download the photo
-    urllib.request.urlretrieve(result['imageurl'], f"./{photo_id}.jpg")
+# Download the photo
+urllib.request.urlretrieve(result['imageurl'], f"./{photo_id}.jpg")
 
-    # Upload the photo to twitter
-    media = twitter_API.media_upload(f"./{photo_id}.jpg")
+# Upload the photo to twitter
+media = twitter_API.media_upload(f"./{photo_id}.jpg")
 
-    # Assemble the tweet
-    date = extract_date(result["date"])
-    summary = get_first_sentence(result["summary"])
-    if len(summary + " " + date) > 280:
-        summary = summary[:277 - (len(date) + 1)] + '...'
+# Assemble the tweet
+date = extract_date(result["date"])
+summary = get_first_sentence(result["summary"])
+if len(summary + " " + date) > 280:
+    summary = summary[:277 - (len(date) + 1)] + '...'
 
-    tweet = summary + " " + date + " " + result['pageurl']
+tweet = summary + " " + date + " " + result['pageurl']
 
-    # Tweet
-    twitter_API.update_status(status=tweet, media_ids=[media.media_id])
+# Tweet
+response = client.create_tweet(
+    text=tweet,
+    media_ids=[media.media_id]
+)
 
-    # Remove the photo file
-    os.remove(f"./{photo_id}.jpg")
+# Remove the photo file
+os.remove(f"./{photo_id}.jpg")
 
-    print(f"Tweet made: {tweet}")
+print(f"Tweet made: {tweet}")
