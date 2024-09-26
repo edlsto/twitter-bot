361c91afbcb56d0b574db4de92cc381f4521ef45
Edward Stoner <edwardlstoner@gmail.com>
2024-09-26 14:41:48 -0600

    fix easter function

--- date_utils.py
+++ date_utils.py
@@ -19,18 +19,17 @@ def get_current_holiday():
     return None
 
 def calculate_easter(year):
-    """Calculate Easter date for a given year."""
     a = year % 19
     b = year // 100
     c = year % 100
     d = b // 4
     e = b % 4
     f = (b + 8) // 25
-    g = (b - f + 1) // 16
+    g = (b - f + 1) // 3
     h = (19 * a + b - d - g + 15) % 30
-    j = c // 16
-    k = c % 16
-    l = (32 + 2 * e + 2 * h - k) % 7
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
     m = (a + 11 * h + 22 * l) // 451
     month = (h + l - 7 * m + 114) // 31
     day = ((h + l - 7 * m + 114) % 31) + 1
